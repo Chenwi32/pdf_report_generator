fe2d8a1938aba9ac74a9d928b7356d79843e7522
chenwi32 <chenwieugene.j@gmail.com>
2023-04-06 05:59:15 +0100

    created a getstaticprops function

--- pages/index.js
+++ pages/index.js
@@ -12,13 +12,14 @@ import {
 import dynamic from "next/dynamic";
 import Head from "next/head";
 import { useRef } from "react";
+import prisma from "../lib/prisma";
 
 const Generatepdf = dynamic(() => import("../components/generatepdf"), {
   //this deactivates server side rendering since we need to initialize this only in the frontend
   ssr: false,
 });
 
-export default function Home() {
+export default function Home({ student }) {
   const ref = useRef();
 
   return (
@@ -28,102 +29,46 @@ export default function Home() {
       </Head>
       <Container mt={10} id="table" ref={ref} maxW={1200}>
         <TableContainer>
-          <Table colorScheme="teal" id="table_content">
-            <Thead m={20} bg="blue.100">
+          <Table id="table_content">
+            <Thead m={20} bg="black" color="white">
               <Tr>
-                <Th border="2px">To convert</Th>
-                <Th border="2px">into</Th>
+                <Th border="2px">
+                  Subject / Tutor's name <br /> Discipline et Nom du Prof
+                </Th>
+                <Th border="2px">
+                  5<sup>th</sup> seq
+                </Th>
+                <Th border="2px">
+                  6<sup>th</sup> seq
+                </Th>
                 <Th border="2px" isNumeric>
-                  multiply by
+                  Term, AV <br /> Moy. Tr
+                </Th>
+                <Th border="2px" isNumeric>
+                  Coeff
+                </Th>
+                <Th border="2px" isNumeric>
+                  Total <br /> (NXC)
+                </Th>
+                <Th border="2px" isNumeric>
+                  position <br /> Rang
+                </Th>
+                <Th border="2px" isNumeric>
+                  Tutor's remark <br /> Signature
                 </Th>
               </Tr>
             </Thead>
             <Tbody>
               <Tr>
-                <Td border="1px">inches</Td>
-                <Td border="1px">millimetres (mm)</Td>
-                <Td border="1px" isNumeric>
-                  25.4
-                </Td>
+                <Td border="1px">English / Victorine </Td>
+                <Td border="1px">16</Td>
+                <Td border="1px">14</Td>
+                <Td border="1px">12</Td>
+                <Td border="1px">3</Td>
+                <Td border="1px">30</Td>
+                <Td border="1px">5<sup>th</sup></Td>
+                <Td border="1px">Fair</Td>
               </Tr>
-              <Tr>
-                <Td border="1px">feet</Td>
-                <Td border="1px">centimetres (cm)</Td>
-                <Td border="1px" isNumeric>
-                  30.48
-                </Td>
-              </Tr>
-              <Tr>
-                <Td border="1px">yards</Td>
-                <Td border="1px">metres (m)</Td>
-                <Td border="1px" isNumeric>
-                  0.91444
-                </Td>
-              </Tr>
-              <Tr>
-                <Td border="1px">yards</Td>
-                <Td border="1px">metres (m)</Td>
-                <Td border="1px" isNumeric>
-                  0.91444
-                </Td>
-              </Tr>
-              <Tr>
-                <Td border="1px">yards</Td>
-                <Td border="1px">metres (m)</Td>
-                <Td border="1px" isNumeric>
-                  0.91444
-                </Td>
-              </Tr>
-              <Tr>
-                <Td border="1px">yards</Td>
-                <Td border="1px">metres (m)</Td>
-                <Td border="1px" isNumeric>
-                  0.91444
-                </Td>
-              </Tr>
-              <Tr>
-                <Td border="1px">yards</Td>
-                <Td border="1px">metres (m)</Td>
-                <Td border="1px" isNumeric>
-                  0.91444
-                </Td>
-              </Tr>
-              <Tr>
-                <Td border="1px">yards</Td>
-                <Td border="1px">metres (m)</Td>
-                <Td border="1px" isNumeric>
-                  0.91444
-                </Td>
-              </Tr>
-              <Tr>
-                <Td border="1px">yards</Td>
-                <Td border="1px">metres (m)</Td>
-                <Td border="1px" isNumeric>
-                  0.91444
-                </Td>
-              </Tr>
-              <Tr>
-                <Td border="1px">yards</Td>
-                <Td border="1px">metres (m)</Td>
-                <Td border="1px" isNumeric>
-                  0.91444
-                </Td>
-              </Tr>
-              <Tr>
-                <Td border="1px">yards</Td>
-                <Td border="1px">metres (m)</Td>
-                <Td border="1px" isNumeric>
-                  0.91444
-                </Td>
-              </Tr>
-              <Tr>
-                <Td border="1px">yards</Td>
-                <Td border="1px">metres (m)</Td>
-                <Td border="1px" isNumeric>
-                  0.91444
-                </Td>
-              </Tr>
-             
             </Tbody>
           </Table>
         </TableContainer>
@@ -132,3 +77,12 @@ export default function Home() {
     </>
   );
 }
+
+export const getStaticProps = async () => {
+  const Student_record = await prisma.Student_record.findMany();
+  console.log(Student_record);
+  return {
+    props: { Student_record },
+    revalidate: 10,
+  };
+};
