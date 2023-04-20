3424102748e50dfcd5f1345274f0714bc8148a27
chenwi32 <chenwieugene.j@gmail.com>
2023-04-21 00:03:29 +0100

    Eugene

--- pages/students.jsx
+++ pages/students.jsx
@@ -1,4 +1,4 @@
-import { Button, Container, Flex, HStack, Input, Table, TableContainer, Tbody, Td, Text, Th, Thead, Tr, VStack } from "@chakra-ui/react";
+import { Button, Container, Flex, HStack, Heading, Input, Table, TableContainer, Tbody, Td, Text, Th, Thead, Tr, VStack } from "@chakra-ui/react";
 import { useEffect, useRef, useState } from "react";
 import prisma from "../lib/prisma";
 
@@ -21,13 +21,13 @@ const Students = (props) => {
   useEffect(() => {
     
    handleSearch();
-   
+
   }, [studentClass])
   
 
     return (
       <Container ref={ref} maxW={1200}>
-        <Flex flexDirection={'column'}>
+        <Flex flexDirection={"column"}>
           <Text mb={5}>Which class do you want?</Text>
 
           <Flex mb={5} gap={5}>
@@ -38,52 +38,56 @@ const Students = (props) => {
             <Button onClick={() => setStudentClass("Form 5")}>Form 5</Button>
           </Flex>
         </Flex>
+        {classStudents.length === 0 ? (
+          <Heading fontSize={"1rem"}>
+            Sorry no students have been registered in this class Yet
+          </Heading>
+        ) :
+          <TableContainer textAlign={"center"}>
+            <Table id="table_content">
+              <Thead m={20} bg="black">
+                <Tr>
+                  <Th color="white" border="2px">
+                    Student ID
+                  </Th>
+                  <Th color="white" border="2px">
+                    Student Name
+                  </Th>
+                  <Th color="white" border="2px">
+                    Class
+                  </Th>
+                  <Th color="white" border="2px">
+                    Date of birth
+                  </Th>
+                  <Th color="white" border="2px">
+                    Place of birth
+                  </Th>
+                  <Th color="white" border="2px">
+                    Status
+                  </Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {classStudents.map((student) => {
+                  const date = student.date_of_birth.toString().substring(0, 10);
+                  const repeater = student.repeater === true;
 
-        <TableContainer textAlign={"center"}>
-          <Table id="table_content">
-            <Thead m={20} bg="black">
-              <Tr>
-                <Th color="white" border="2px">
-                  Student ID
-                </Th>
-                <Th color="white" border="2px">
-                  Student Name
-                </Th>
-                <Th color="white" border="2px">
-                  Class
-                </Th>
-                <Th color="white" border="2px">
-                  Date of birth
-                </Th>
-                <Th color="white" border="2px">
-                  Place of birth
-                </Th>
-                <Th color="white" border="2px">
-                  Status
-                </Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              {classStudents.map((student) => {
-                const date = student.date_of_birth.toString().substring(0, 10);
-                const repeater = student.repeater === true;
-
-                return (
-                  <Tr key={student.student_id}>
-                    <Td border="1px">{student.student_id}</Td>
-                    <Td border="1px">{student.student_name}</Td>
-                    <Td border="1px">{student.student_class}</Td>
-                    <Td border="1px">{date}</Td>
-                    <Td border="1px">{student.place_of_birth}</Td>
-                    <Td border="1px">
-                      {repeater ? "Repeater" : "New Student"}{" "}
-                    </Td>
-                  </Tr>
-                );
-              })}
-            </Tbody>
-          </Table>
-        </TableContainer>
+                  return (
+                    <Tr key={student.student_id}>
+                      <Td border="1px">{student.student_id}</Td>
+                      <Td border="1px">{student.student_name}</Td>
+                      <Td border="1px">{student.student_class}</Td>
+                      <Td border="1px">{date}</Td>
+                      <Td border="1px">{student.place_of_birth}</Td>
+                      <Td border="1px">
+                        {repeater ? "Repeater" : "New Student"}{" "}
+                      </Td>
+                    </Tr>
+                  );
+                })}
+              </Tbody>
+            </Table>
+          </TableContainer>}
       </Container>
     );
 }
